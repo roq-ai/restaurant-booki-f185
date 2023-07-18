9b043097109b2f4f314a956b1326f8b6917113fe
roq-ai <>
2023-07-18 15:42:05 +0000

    Initial commit

---
+++ src/config/app.config.ts
@@ -0,0 +1,16 @@
+interface AppConfigInterface {
+  ownerRoles: string[];
+  customerRoles: string[];
+  tenantRoles: string[];
+  tenantName: string;
+  applicationName: string;
+  addOns: string[];
+}
+export const appConfig: AppConfigInterface = {
+  ownerRoles: ['Owner'],
+  customerRoles: ['Guest'],
+  tenantRoles: ['Owner', 'Chef', 'Waiter'],
+  tenantName: 'Restaurant',
+  applicationName: 'Restaurant booking engine',
+  addOns: ['notifications', 'file upload', 'chat', 'file'],
+};
---
+++ src/pages/reservations/edit/[id]/index.tsx
@@ -0,0 +1,179 @@
+import AppLayout from 'layout/app-layout';
+import React, { useState } from 'react';
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Text,
+  Box,
+  Spinner,
+  FormErrorMessage,
+  Switch,
+  NumberInputStepper,
+  NumberDecrementStepper,
+  NumberInputField,
+  NumberIncrementStepper,
+  NumberInput,
+  Center,
+} from '@chakra-ui/react';
+import * as yup from 'yup';
+import DatePicker from 'react-datepicker';
+import { FiEdit3 } from 'react-icons/fi';
+import { useFormik, FormikHelpers } from 'formik';
+import { getReservationById, updateReservationById } from 'apiSdk/reservations';
+import { Error } from 'components/error';
+import { reservationValidationSchema } from 'validationSchema/reservations';
+import { ReservationInterface } from 'interfaces/reservation';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import { AsyncSelect } from 'components/async-select';
+import { ArrayFormField } from 'components/array-form-field';
+import { AccessOperationEnum, AccessServiceEnum, requireNextAuth, withAuthorization } from '@roq/nextjs';
+import { compose } from 'lib/compose';
+import { UserInterface } from 'interfaces/user';
+import { RestaurantInterface } from 'interfaces/restaurant';
+import { getUsers } from 'apiSdk/users';
+import { getRestaurants } from 'apiSdk/restaurants';
+
+function ReservationEditPage() {
+  const router = useRouter();
+  const id = router.query.id as string;
+  const { data, error, isLoading, mutate } = useSWR<ReservationInterface>(
+    () => (id ? `/reservations/${id}` : null),
+    () => getReservationById(id),
+  );
+  const [formError, setFormError] = useState(null);
+
+  const handleSubmit = async (values: ReservationInterface, { resetForm }: FormikHelpers<any>) => {
+    setFormError(null);
+    try {
+      const updated = await updateReservationById(id, values);
+      mutate(updated);
+      resetForm();
+      router.push('/reservations');
+    } catch (error) {
+      setFormError(error);
+    }
+  };
+
+  const formik = useFormik<ReservationInterface>({
+    initialValues: data,
+    validationSchema: reservationValidationSchema,
+    onSubmit: handleSubmit,
+    enableReinitialize: true,
+    validateOnChange: false,
+    validateOnBlur: false,
+  });
+
+  return (
+    <AppLayout>
+      <Box bg="white" p={4} rounded="md" shadow="md">
+        <Box mb={4}>
+          <Text as="h1" fontSize="2xl" fontWeight="bold">
+            Edit Reservation
+          </Text>
+        </Box>
+        {error && (
+          <Box mb={4}>
+            <Error error={error} />
+          </Box>
+        )}
+        {formError && (
+          <Box mb={4}>
+            <Error error={formError} />
+          </Box>
+        )}
+        {isLoading || (!formik.values && !error) ? (
+          <Center>
+            <Spinner />
+          </Center>
+        ) : (
+          <form onSubmit={formik.handleSubmit}>
+            <FormControl id="date" mb="4">
+              <FormLabel>Date</FormLabel>
+              <Box display="flex" maxWidth="100px" alignItems="center">
+                <DatePicker
+                  dateFormat={'dd/MM/yyyy'}
+                  selected={formik.values?.date ? new Date(formik.values?.date) : null}
+                  onChange={(value: Date) => formik.setFieldValue('date', value)}
+                />
+                <Box zIndex={2}>
+                  <FiEdit3 />
+                </Box>
+              </Box>
+            </FormControl>
+            <FormControl id="time" mb="4">
+              <FormLabel>Time</FormLabel>
+              <Box display="flex" maxWidth="100px" alignItems="center">
+                <DatePicker
+                  dateFormat={'dd/MM/yyyy'}
+                  selected={formik.values?.time ? new Date(formik.values?.time) : null}
+                  onChange={(value: Date) => formik.setFieldValue('time', value)}
+                />
+                <Box zIndex={2}>
+                  <FiEdit3 />
+                </Box>
+              </Box>
+            </FormControl>
+            <FormControl id="guest_count" mb="4" isInvalid={!!formik.errors?.guest_count}>
+              <FormLabel>Guest Count</FormLabel>
+              <NumberInput
+                name="guest_count"
+                value={formik.values?.guest_count}
+                onChange={(valueString, valueNumber) =>
+                  formik.setFieldValue('guest_count', Number.isNaN(valueNumber) ? 0 : valueNumber)
+                }
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              {formik.errors.guest_count && <FormErrorMessage>{formik.errors?.guest_count}</FormErrorMessage>}
+            </FormControl>
+            <AsyncSelect<UserInterface>
+              formik={formik}
+              name={'user_id'}
+              label={'Select User'}
+              placeholder={'Select User'}
+              fetcher={getUsers}
+              renderOption={(record) => (
+                <option key={record.id} value={record.id}>
+                  {record?.email}
+                </option>
+              )}
+            />
+            <AsyncSelect<RestaurantInterface>
+              formik={formik}
+              name={'restaurant_id'}
+              label={'Select Restaurant'}
+              placeholder={'Select Restaurant'}
+              fetcher={getRestaurants}
+              renderOption={(record) => (
+                <option key={record.id} value={record.id}>
+                  {record?.name}
+                </option>
+              )}
+            />
+            <Button isDisabled={formik?.isSubmitting} colorScheme="blue" type="submit" mr="4">
+              Submit
+            </Button>
+          </form>
+        )}
+      </Box>
+    </AppLayout>
+  );
+}
+
+export default compose(
+  requireNextAuth({
+    redirectTo: '/',
+  }),
+  withAuthorization({
+    service: AccessServiceEnum.PROJECT,
+    entity: 'reservation',
+    operation: AccessOperationEnum.UPDATE,
+  }),
+)(ReservationEditPage);
